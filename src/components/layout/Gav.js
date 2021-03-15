b9473085f2e50e8f0ee4cd6ef2e1c23f3b590517
David Summers <djs327@newcastle.edu.au>
2021-03-15 15:54:47 +1100

    Gav experiments

--- src/components/layout/Gav.js
+++ src/components/layout/Gav.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Scrollbars } from 'rc-scrollbars';
+
+const [question,setQuestion] = 'type inquiry here';
+
+const handleQuestion = (question) => {
+return (`text box ${question}`);
+}
+
 
 const Gav = () => {
     return (
         <div >
-            <Scrollbars style={{ width: 700, height: 300 }}>
-            <p className='textbubble'>Hi, I'm <strong>Gav</strong> a know the law bot that can help you find what your looking for. Type in what you want to talk about.</p>
+            <Scrollbars style={{ width: 1200, height: 300 }}>
+            <p className='gavbubble'>Hi, I'm <strong>Gav</strong> a know the law bot that can help you find what your looking for. Type in what you want to talk about.</p>
             </Scrollbars>
+            <form>
+                <input type="text" name='question' onChange={handleQuestion} />
+            </form>
             
         </div>
     )
